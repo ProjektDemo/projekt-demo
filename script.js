f38a8a80d0874da8a261cbfbd459fa1cf86f3b1c
ProjektDemo <projekt.demo@interia.pl>
2026-08-26 19:40:25 +0200

    Usunięcie wszystkich plików. Nowy serwer

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector("#test-button");
-const message = document.querySelector("#test-message");
-
-button.addEventListener("click", function () {
-  message.textContent = "JavaScript działa.";
-});
